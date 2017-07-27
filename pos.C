2220b30beabb03baab90d5f7b0a2602f2fbfbed7
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-07-27 10:25:17 +0530

    Create pos.C

---
+++ pos.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a==0)
+  printf("ZERO");
+if(a>0)
+  printf("Positive");
+if(a<0)
+   printf("Negative");
+}
